e4ae2bb1eb900b2435eb109102c3529867da60e6
Geovanne Vichiatto <vichiattoge@gmail.com>
2021-08-18 10:23:35 -0300

    Geração automática de Ids

--- src/main/java/br/com/gevichiatto/sisgu_angular/entity/Cargos.java
+++ src/main/java/br/com/gevichiatto/sisgu_angular/entity/Cargos.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Cargos {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String nome;
--- src/main/java/br/com/gevichiatto/sisgu_angular/entity/Perfis.java
+++ src/main/java/br/com/gevichiatto/sisgu_angular/entity/Perfis.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Perfis {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String nome;

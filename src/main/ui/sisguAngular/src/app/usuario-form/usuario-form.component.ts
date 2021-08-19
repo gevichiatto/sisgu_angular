a4273090a62b399e114b12f72f2167bc3dc336e5
Geovanne Vichiatto <vichiattoge@gmail.com>
2021-08-19 01:22:26 -0300

    Formulário de usuário.

--- src/main/ui/sisguAngular/src/app/usuario-form/usuario-form.component.ts
+++ src/main/ui/sisguAngular/src/app/usuario-form/usuario-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Cargo } from '../classes/cargo';
+import { Perfil } from '../classes/perfil';
+import { Usuario } from '../classes/usuario';
+import { CargoService } from '../services/cargo.service';
+import { PerfilService } from '../services/perfil.service';
 
 @Component({
   selector: 'app-usuario-form',
@@ -7,9 +12,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioFormComponent implements OnInit {
 
-  constructor() { }
+  cargos!: Cargo[];
+  perfis!: Perfil[];
 
-  ngOnInit(): void {
+  nomeUsuario!: string;
+  cpfUsuario!: string;
+  dataNascimento!: Date;
+  sexo!: string;
+  nomeCargo!: string;
+  nomePerfil!: string;
+
+  cargoObj!: Cargo;
+  perfilObj!: Perfil;
+
+  usuario!: Usuario;
+
+  constructor(private perfilService: PerfilService, private cargoService: CargoService) {
+    this.usuario = new Usuario();
   }
+
+  ngOnInit() {
+    this.perfilService.findAll().subscribe(data => {
+      data.sort(function(a,b) { 
+        if (a.nome >= b.nome)
+          return 1;
+        else
+          return -1;
+      });
+      this.perfis = data;
+      this.cargoService.findAll().subscribe(data => {
+        data.sort(function(a,b) { 
+          if (a.nome >= b.nome)
+            return 1;
+          else
+            return -1;
+        });
+        this.cargos = data;
+      });
+    });
+  }
+
+  onSubmit() {
+    for (let p of this.perfis) {
+      if (p.nome == this.nomePerfil) {
+        this.perfilObj = p;
+      }
+    }
+
+    for (let c of this.cargos) {
+      if (c.nome == this.nomeCargo) {
+        this.cargoObj = c;
+      }
+    }
+    
+    this.usuario.nome = this.nomeUsuario;
+    this.usuario.cpf = this.cpfUsuario;
+    this.usuario.dataNascimento = new Date(this.dataNascimento);
+    this.usuario.sexo = this.sexo;
+    this.usuario.perfil = this.perfilObj;
+    this.usuario.cargo = this.cargoObj;
+    this.usuario.dataCadastro = new Date();
+
+    console.log("Usuario: ", this.usuario);
+
+  }
+
 
 }
